690df7a9fb3334ba7f2780fd7e758cdc20cde816
berdibek-2002 <uralov0212@gmail.com>
2024-09-17 10:36:47 +0500

    version 1.0.3.3

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -6,7 +6,7 @@ const Header = (props: HeaderProps) => {
   return (
     <div className='w-full py-3 px-4 h-auto bg-gray-100 dark:bg-gray-900 border-b'>
       <h1 className='text-3xl font-semibold text-primary dark:text-white'>Welcome</h1>
-      <p className='text-xs font-normal text-primary dark:text-white'>Berdibek Uralov</p>
+      <p className='text-xs font-normal text-primary dark:text-white'></p>
     </div>
   );
 };
